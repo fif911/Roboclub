93ae5e54655e612a057feb2c1f27b4694b2e655d
Alex Zakotyanskiy <sandrrichfif911@gmail.com>
2017-05-30 14:51:54 +0300

    trying to fix Heroku DB(added static settings)

--- robo/settings.py
+++ robo/settings.py
@@ -134,10 +134,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
-#when deploying uncomment
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# STATIC_URL = '/static/'
+# # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+# #when deploying uncomment
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "upload")
 
